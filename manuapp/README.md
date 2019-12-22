5e406d7eb04a74c10e69814e096ae5e41a8530de
lucutovidiu <lucut_ovidiu@yahoo.com>
2019-12-22 21:28:41 +0200

    new project serverside rendered

---
+++ manuapp/README.md
@@ -0,0 +1,13 @@
+# Manuapp
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.20.
+
+# If watcher problems run below
+sudo sysctl fs.inotify.max_user_watches=582222 && sudo sysctl -p
+
+## How was the project installed with Nest / Angular Universal
+ng new manuapp && cd manuapp && ng add @nest/ng-universal
+
+
+# In the server folder can add API's
+nest new controller ...
--- manuapp/src/app/app.component.spec.ts
+++ manuapp/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'client'`, () => {
+  it(`should have as title 'manuapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('client');
+    expect(app.title).toEqual('manuapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('client app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('manuapp app is running!');
   });
 });
