71ac72e9154580d054f5af61b8f694a536e44f9e
lucutovidiu <lucut_ovidiu@yahoo.com>
2020-01-07 23:36:26 +0200

    add home and about route working

--- manuapp/src/app/core_modules/navbar/navbar.module.ts
+++ manuapp/src/app/core_modules/navbar/navbar.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterModule } from '@angular/router';
 
 import { MenuBarComponent } from './menu/menu.component'
 
 @NgModule({
   declarations: [MenuBarComponent],
   imports: [
-    CommonModule, BrowserAnimationsModule, MatMenuModule, MatButtonModule
+    CommonModule, BrowserAnimationsModule, MatMenuModule, MatButtonModule,RouterModule
   ],
   exports:[MenuBarComponent]
 })
