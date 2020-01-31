df862e76e44f2d938585ee0545633d11b5295a94
Ovidiu Lucut <ovidiu.lucut@fortech.ro>
2020-01-31 23:26:15 +0200

    added login component UI

--- src/app/page-modules/login-page/login-page.module.ts
+++ src/app/page-modules/login-page/login-page.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 import { LoginComponent } from './login/login.component';
 import { LoginPageRoutingModule } from './login-page-routing.module';
+import { SharedServicesModuleModule } from 'shared_services/shared-services-module.module'
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    SharedServicesModuleModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class LoginPageModule { }
